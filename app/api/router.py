d678e630c630cb26d072053f9f9761bbdbf353b0
Philipe Da Motta Mello <38898462+Philipessj1@users.noreply.github.com>
2026-06-22 20:06:43 -0300

    feat: add protected current user endpoint

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, health
+from app.api.routes import auth, health, users
 
 # Define main API router
 api_router = APIRouter()
@@ -8,3 +8,4 @@ api_router = APIRouter()
 # Include all routes
 api_router.include_router(auth.router)
 api_router.include_router(health.router)
+api_router.include_router(users.router)
